4c2abd2f8d8679c3379d75f4b48e293b87357b0f
Dahye Oh <hi.yogicat@gmail.com>
2022-03-04 16:18:46 +0900

    feat: :fire: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { useQuery } from 'react-query'
+import axios from 'axios'
 
 function App() {
-  console.log(useQuery)
+  const queryInfo = useQuery<PokemonQuery>('pokemon', () =>
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then((res) => res.data.results)
+  )
 
-  return <div className="App">Hello React Query!</div>
+  console.log(queryInfo)
+
+  return (
+    <div>
+      <h1>Hello React Query</h1>
+      {queryInfo.data.map((result) => (
+        <div key={result.name}>{result.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export default App
